10eba395a02a93102800bfa79ac0521f638eaa50
Ekaterina Smagina <i.sealexa@yandex.ru>
2022-06-23 02:29:13 +0300

    plus three tests for incorrect login

--- src/test/java/user/LoginUserNegativeTest.java
+++ src/test/java/user/LoginUserNegativeTest.java
@@ -4,10 +4,13 @@ import client.UserClient;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
 public class LoginUserNegativeTest {
+
     private UserClient userClient;
     private int expectedCode;
     private String expectedMessage;
@@ -21,9 +24,9 @@ public class LoginUserNegativeTest {
 
     @Test
     public void loginIsNotExistingUserHasToReturnError(){
-        User user = new User("userDoesNtExist@testDomain.test", "qwe456");
+        User userData = new User("userDoesNtExist@testDomain.test", "qwe456");
 
-        ValidatableResponse response = userClient.login(user);
+        ValidatableResponse response = userClient.login(userData);
         int actualCode = response.extract().statusCode();
         Token body = response.extract().body().as(Token.class);
         boolean status = body.isSuccess();
@@ -57,5 +60,77 @@ public class LoginUserNegativeTest {
         assertFalse("В ответе вернулось пустое значение в поле message", actualMessage.isBlank());
         assertEquals("В ответе вернулось некорректное значение для поля message", expectedMessage, actualMessage);
         userClient.delete(token);
+    }
+
+    @Test
+    public void  loginExistingUserWithNameAndPasswordHasToReturnError(){
+        User user = User.getRandomUser();
+        String password = user.getPassword();
+        String name = user.getName();
+        User userData = new User(null, password, name);
+
+        ValidatableResponse register = userClient.register(user);
+        String token = register.extract().body().as(Token.class).getAccessToken();
+        ValidatableResponse response = userClient.login(userData);
+
+        int actualCode = response.extract().statusCode();
+        Token body = response.extract().body().as(Token.class);
+        boolean status = body.isSuccess();
+        String actualMessage = body.getMessage();
+
+        assertNotNull("В ответе вернулось пустое Body", body);
+        assertEquals("В ответе вернулся другой код состояния", expectedCode, actualCode);
+        assertFalse("В ответе вернулось некорректное значение для поля success", status);
+        assertFalse("В ответе вернулось пустое значение в поле message", actualMessage.isBlank());
+        assertEquals("В ответе вернулось некорректное значение для поля message", expectedMessage, actualMessage);
+        userClient.delete(token);
+    }
+
+    @Test
+    public void  loginExistingUserWithIncorrectPasswordHasToReturnError(){
+        User user = User.getRandomUser();
+        String email = user.getEmail();
+        User userData = new User(email, "incorrect");
+
+        ValidatableResponse register = userClient.register(user);
+        String token = register.extract().body().as(Token.class).getAccessToken();
+        ValidatableResponse response = userClient.login(userData);
+
+        int actualCode = response.extract().statusCode();
+        Token body = response.extract().body().as(Token.class);
+        boolean status = body.isSuccess();
+        String actualMessage = body.getMessage();
+
+        assertNotNull("В ответе вернулось пустое Body", body);
+        assertEquals("В ответе вернулся другой код состояния", expectedCode, actualCode);
+        assertFalse("В ответе вернулось некорректное значение для поля success", status);
+        assertFalse("В ответе вернулось пустое значение в поле message", actualMessage.isBlank());
+        assertEquals("В ответе вернулось некорректное значение для поля message", expectedMessage, actualMessage);
+        userClient.delete(token);
+    }
+
+    @Test
+    public void  loginDeletedUserHasToReturnError(){
+        User user = User.getRandomUser();
+        String email = user.getEmail();
+        String password = user.getPassword();
+        User userData = new User(email, password);
+
+        ValidatableResponse register = userClient.register(user);
+        String token = register.extract().body().as(Token.class).getAccessToken();
+        userClient.delete(token);
+
+        ValidatableResponse response = userClient.login(userData);
+
+        int actualCode = response.extract().statusCode();
+        Token body = response.extract().body().as(Token.class);
+        boolean status = body.isSuccess();
+        String actualMessage = body.getMessage();
+
+        assertNotNull("В ответе вернулось пустое Body", body);
+        assertEquals("В ответе вернулся другой код состояния", expectedCode, actualCode);
+        assertFalse("В ответе вернулось некорректное значение для поля success", status);
+        assertFalse("В ответе вернулось пустое значение в поле message", actualMessage.isBlank());
+        assertEquals("В ответе вернулось некорректное значение для поля message", expectedMessage, actualMessage);
     }
 }
