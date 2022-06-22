15ddf6476c666758922ab537bbc0af9cd812d574
Ekaterina Smagina <i.sealexa@yandex.ru>
2022-06-23 00:58:42 +0300

    create new test for register user which was deleted

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -48,6 +48,7 @@ public class UserTest {
         assertTrue("В ответе поле accessToken должно было начинаться с Bearer", body.getAccessToken().startsWith("Bearer"));
         assertFalse("В ответе вернулось пустое значение в поле refreshToken", body.getRefreshToken().isBlank());
     }
+
     @Test
     public void registerTheSameUserTwiceHasToReturnError() {
         User user = User.getRandomUser();
@@ -66,5 +67,35 @@ public class UserTest {
         assertFalse("В ответе вернулось некорректное значение для поля success", status);
         assertFalse("В ответе вернулось пустое значение в поле message", actualMessage.isBlank());
         assertEquals("В ответе вернулось некорректное значение для поля message", expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void registerDeletedUserHasToCreateUser() {
+        User user = User.getRandomUser();
+        int expectedCode = 200;
+        String expectedEmail = user.getEmail().toLowerCase(Locale.ROOT);
+        String expectedName = user.getName();
+
+        token = userClient.register(user).extract().body().as(Token.class).getAccessToken();
+        ValidatableResponse deletedResponse = userClient.delete(token);
+        assertEquals("При удалении юзера в ответе вернулся другой код состояния", 202, deletedResponse.extract().statusCode());
+        assertTrue("При удалении юзера в ответе вернулось некорректное значение для поля success", deletedResponse.extract().body().as(Token.class).isSuccess());
+
+        ValidatableResponse response = userClient.register(user);
+        int actualCode = response.extract().statusCode();
+        Token body = response.extract().body().as(Token.class);
+        boolean status = body.isSuccess();
+        String actualEmail = body.getUser().getEmail();
+        String actualName = body.getUser().getName();
+        token = body.getAccessToken();
+
+        assertNotNull("В ответе вернулось пустое Body", body);
+        assertEquals("В ответе вернулся другой код состояния", expectedCode, actualCode);
+        assertTrue("В ответе вернулось некорректное значение для поля success", status);
+        assertEquals("В ответе вернулось некорректное значение для поля email", expectedEmail, actualEmail);
+        assertEquals("В ответе вернулось некорректное значение для поля name", expectedName, actualName);
+        assertFalse("В ответе вернулось пустое значение в поле accessToken", body.getAccessToken().isBlank());
+        assertTrue("В ответе поле accessToken должно было начинаться с Bearer", body.getAccessToken().startsWith("Bearer"));
+        assertFalse("В ответе вернулось пустое значение в поле refreshToken", body.getRefreshToken().isBlank());
     }
 }
