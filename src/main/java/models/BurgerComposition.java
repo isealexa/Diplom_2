5b4a39ec5449174da5e6a92817452d15d3acc040
Ekaterina Smagina <i.sealexa@yandex.ru>
2022-06-26 17:14:43 +0300

    added checks and optimized the class OrderNoAuthTest

--- src/main/java/models/BurgerComposition.java
+++ src/main/java/models/BurgerComposition.java
@@ -1,11 +1,13 @@
 package models;
 
+//захардкоженные данные для получения ингрилиентов, цены и названия,
+//на случай, если get-запрос на получение ингридиентов бдует не работать
 public class BurgerComposition {
 
     public String getSuperBurger(){
         return "{\"ingredients\":[" +
-                //bun: Флюоресцентная булка R2-D3
-                "\"61c0c5a71d1f82001bdaaa6d\"," +
+                //bun: Флюоресцентная булка R2-D3б, Краторная булка N-200i
+                "\"61c0c5a71d1f82001bdaaa6d\",\"61c0c5a71d1f82001bdaaa6c\"," +
                 //main: Сыр с астероидной плесенью, Мини-салат Экзо-Плантаго, Плоды Фалленианского дерева,
                 "\"61c0c5a71d1f82001bdaaa7a\",\"61c0c5a71d1f82001bdaaa79\",\"61c0c5a71d1f82001bdaaa77\"," +
                 //sauce: Соус с шипами Антарианского плоскоходца, Соус Spicy-X
@@ -53,18 +55,78 @@ public class BurgerComposition {
     }
 
     public String getDoubleBun(){
-        return "{\"ingredients\":[\"61c0c5a71d1f82001bdaaa6c\",\"61c0c5a71d1f82001bdaaa6d\"]}"; //bun: Краторная булка N-200i, Флюоресцентная булка R2-D3
-    }
-
-    public String getNoIngredients(){
-        return "{\"ingredients\":[]}";
-    }
-
-    public String getEmptyJson(){
-        return "{}";
+        return "{\"ingredients\":[\"61c0c5a71d1f82001bdaaa6c\",\"61c0c5a71d1f82001bdaaa6c\"]}"; //bun: Краторная булка N-200i 2 шт
     }
 
     public String getIncorrect(){
         return "{\"ingredients\":[\"61c01bi7y70jnkj001bdaa\"]}";
+    }
+
+    public Integer getPrice(String burger){
+        Integer price;
+
+        switch (burger){
+            case "super":
+                price = 13249;
+                break;
+            case "big":
+                price = 3491;
+                break;
+            case "standard":
+                price = 4270;
+                break;
+            case "mini":
+                price = 1679;
+                break;
+            case "bun":
+                price = 1255;
+                break;
+            case "main":
+                price = 1337;
+                break;
+            case "sauce":
+                price = 15;
+                break;
+            case "double bun":
+                price = 2510;
+                break;
+            default: price = null;
+        }
+
+        return price;
+    }
+
+    public String getName(String burger){
+        String name;
+
+        switch (burger){
+            case "super":
+                name = "Spicy экзо-плантаго краторный фалленианский люминесцентный антарианский флюоресцентный астероидный био-марсианский бургер";
+                break;
+            case "big":
+                name = "Spicy люминесцентный антарианский флюоресцентный бессмертный бургер";
+                break;
+            case "standard":
+                name = "Метеоритный краторный традиционный-галактический бургер";
+                break;
+            case "mini":
+                name = "Краторный био-марсианский бургер";
+                break;
+            case "bun":
+                name = "Краторный бургер";
+                break;
+            case "main":
+                name = "Бессмертный бургер";
+                break;
+            case "sauce":
+                name = "Традиционный-галактический бургер";
+                break;
+            case "double bun":
+                name = "Краторный бургер";
+                break;
+            default: name = null;
+        }
+
+        return name;
     }
 }
--- src/test/java/order/OrderNoAuthTest.java
+++ src/test/java/order/OrderNoAuthTest.java
@@ -8,11 +8,31 @@ import io.restassured.response.ValidatableResponse;
 import models.BurgerComposition;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class OrderNoAuthTest {
+
+    private final String burger;
+    private final String expectedName;
+
+    public OrderNoAuthTest(String burger, String expectedName){
+        this.burger = burger;
+        this.expectedName = expectedName;
+    }
+
+    @Parameterized.Parameters(name = "The test {index} checks create order: {1} and without authorization")
+    public static Object[][] getOrderParamNoAuth(){
+        return new Object[][] {
+                {new BurgerComposition().getMain(), new BurgerComposition().getName("main") },
+                {new BurgerComposition().getBigBurger(), new BurgerComposition().getName("big") },
+                {new BurgerComposition().getDoubleBun(), new BurgerComposition().getName("double bun") },
+                {new BurgerComposition().getSauce(), new BurgerComposition().getName("sauce") },
+        };
+    }
 
     private OrderClient orderClient;
 
@@ -23,51 +43,24 @@ public class OrderNoAuthTest {
     }
 
     @Test
-    @DisplayName("Create correct mini burger order without authorization return success")
+    @DisplayName("Create correct order without authorization return success")
     @Description("This tests checks getting success response and creating order when push post-request to create order without authorization and correct data")
-    public void createOrderMiniBurgerWithoutAuthorizationReturnSuccess(){
-        String burger = new BurgerComposition().getMiniBurger();
-        ValidatableResponse newOrder = createOrder(burger);
-        checkResponseAndGetId(newOrder);
-    }
-
-    @Test
-    @DisplayName("Create correct big burger order without authorization return success")
-    @Description("This tests checks getting success response and creating order when push post-request to create order without authorization and correct data")
-    public void createOrderBigBurgerWithoutAuthorizationReturnSuccess(){
-        String burger = new BurgerComposition().getBigBurger();
-        ValidatableResponse newOrder = createOrder(burger);
-        checkResponseAndGetId(newOrder);
-    }
-
-    @Test
-    @DisplayName("Create order return double bun without authorization return success")
-    @Description("This tests checks getting success response and creating order when push post-request to create order without authorization and correct data")
-    public void createOrderDoubleBunWithoutAuthorizationReturnSuccess(){
-        String burger = new BurgerComposition().getDoubleBun();
-        ValidatableResponse newOrder = createOrder(burger);
-        checkResponseAndGetId(newOrder);
-    }
-
-    @Test
-    @DisplayName("Create order return only sauce without authorization return success ")
-    @Description("This tests checks getting success response and creating order when push post-request to create order without authorization and correct data")
-    public void createOrderOnlySauceWithoutAuthorizationReturnSuccess(){
-        String burger = new BurgerComposition().getSauce();
-        ValidatableResponse newOrder = createOrder(burger);
-        checkResponseAndGetId(newOrder);
+    public void createOrderBurgerWithoutAuthorizationReturnSuccess(){
+        ValidatableResponse newOrder = createOrder();
+        checkResponse(newOrder);
     }
 
     @Step("Push post-request to create order without authorization and get response")
-    public ValidatableResponse createOrder(String burger){
+    public ValidatableResponse createOrder(){
         return orderClient.createOrder(burger);
     }
 
-    @Step("Check response fields and get order id")
-    public void checkResponseAndGetId(ValidatableResponse response){
+    @Step("Check response fields")
+    public void checkResponse(ValidatableResponse response){
         assertNotNull("Вернулся невалидный ответ", response);
-        //NewOrder order = response.extract().body().as(NewOrder.class);
+        NewOrder order = response.extract().body().as(NewOrder.class);
         assertTrue("В ответе вернулись некорректные код состояние и статус заказа", response.assertThat().statusCode(200).extract().path("success"));
-        //return order.getOrder().get_id();
+        assertNotNull("В ответе вернулось пустое значение в поле number", order.getOrder().getNumber());
+        assertEquals("Название бургера не соотвествует ожидаемому", expectedName, order.getName());
     }
 }
