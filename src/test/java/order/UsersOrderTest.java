7285ab570e3a180e7b23a86140a3f891964e5acf
Ekaterina Smagina <i.sealexa@yandex.ru>
2022-06-26 18:16:39 +0300

    new tests in UsersOrderTest

--- src/test/java/order/UsersOrderTest.java
+++ src/test/java/order/UsersOrderTest.java
@@ -36,7 +36,7 @@ public class UsersOrderTest {
     }
 
     @Test
-    @DisplayName("Get user's order with authorization return success")
+    @DisplayName("Get user's order with authorization return success and check id and number")
     @Description("This tests checks getting success response and show user's order when push get-request with authorization to see user's order")
     public void getUsersOrderWithAuthorizationReturnSuccess(){
         String burger = new BurgerComposition().getSuperBurger();
@@ -47,9 +47,61 @@ public class UsersOrderTest {
         check(response, orderId, number);
     }
 
+    @Test
+    @DisplayName("Get user's order with authorization return success when there isn't any order")
+    @Description("This tests checks getting success response and show user's order when push get-request with authorization to see user's order")
+    public void getUsersNullOrderWithAuthorizationReturnSuccess(){
+        ValidatableResponse response = getUserOrder();
+        check(response);
+    }
+
+    @Test
+    @DisplayName("Get two user's orders with authorization return success")
+    @Description("This tests checks getting success response and show two user's orders when push get-request with authorization to see user's order")
+    public void getTwoUsersOrdersWithAuthorizationReturnSuccess(){
+        String burger = new BurgerComposition().getStandardBurger();
+        String secondBurger = new BurgerComposition().getBigBurger();
+        createOrder(burger);
+        createOrder(secondBurger);
+        ValidatableResponse response = getUserOrder();
+        check(response, 2);
+    }
+
+    @Test
+    @DisplayName("Get 50 user's orders with authorization return success")
+    @Description("This tests checks getting success response and show 50 user's orders when ware created 50 push get-request with authorization to see 50 user's orders")
+    public void getFiftyUsersOrdersWithAuthorizationReturnSuccess(){
+        String burger = new BurgerComposition().getMiniBurger();
+        int createdCount = 50;
+        int expectedCount = 50;
+        createOrder(burger, createdCount);
+        ValidatableResponse response = getUserOrder();
+        check(response, expectedCount);
+    }
+
+    @Test
+    @DisplayName("Get 50 user's orders with authorization return success")
+    @Description("This tests checks getting success response and show 50 user's orders when ware created more than 50 push get-request with authorization to see user's order")
+    public void getFiftyUsersOrdersWithAuthorizationWhenWereCreatedMoreFiftyReturnSuccess(){
+        String burger = new BurgerComposition().getBun();
+        int createdCount = 51;
+        int expectedCount = 50;
+        createOrder(burger, createdCount);
+        ValidatableResponse response = getUserOrder();
+        check(response, expectedCount);
+
+    }
+
     @Step("Push post-request to create order with authorization")
     public ValidatableResponse createOrder(String burger){
         return orderClient.createOrder(token, burger);
+    }
+
+    @Step("Push post-request to create {count} orders with authorization")
+    public void createOrder(String burger, int count){
+        for(int i = 0; i < count; i++) {
+        orderClient.createOrder(token, burger);
+        }
     }
 
     @Step("Push get-request with authorization to see user's orders")
@@ -67,7 +119,17 @@ public class UsersOrderTest {
         return response.extract().body().as(NewOrder.class).getOrder().getNumber();
     }
 
-    @Step("Check response: status code, orders count, order id and price")
+    @Step("Check response: status code, orders count")
+    public void check(ValidatableResponse response){
+        assertNotNull("Вернулся невалидный ответ", response);
+        assertTrue("В ответе вернулись некорректные код состояния ответа и статус заказа", response.assertThat().statusCode(200).extract().path("success"));
+
+        Order[] orders = response.extract().body().as(Orders.class).getOrders();
+        assertNotNull("В ответе вернулся пустой ответ ", orders);
+        assertEquals("Колиство заказазов в ответе не соотвествует ожидаемому", 0, orders.length);
+    }
+
+    @Step("Check response: status code, orders count, order id and number")
     public void check(ValidatableResponse response, String orderId, Integer number){
         assertNotNull("Вернулся невалидный ответ", response);
         assertTrue("В ответе вернулись некорректные код состояния ответа и статус заказа", response.assertThat().statusCode(200).extract().path("success"));
@@ -78,5 +140,18 @@ public class UsersOrderTest {
         assertFalse("В ответе вернулся пустое значение в поле _id заказа", orders[0].get_id().isBlank());
         assertEquals("Поле id заказаза в ответе не соотвествует ожидаемому", orderId, orders[0].get_id());
         assertEquals("Поле price цена заказа в ответе не соотвествует ожидаемому", number,  orders[0].getNumber());
+    }
+
+
+    @Step("Check response: status code, orders count")
+    public void check(ValidatableResponse response, int count){
+        assertNotNull("Вернулся невалидный ответ", response);
+        assertTrue("В ответе вернулись некорректные код состояния ответа и статус заказа", response.assertThat().statusCode(200).extract().path("success"));
+
+        Order[] orders = response.extract().body().as(Orders.class).getOrders();
+        assertNotNull("В ответе вернулся пустой ответ ", orders);
+        assertEquals("Колиство заказазов в ответе не соотвествует ожидаемому", count, orders.length);
+        assertFalse("В ответе вернулся пустое значение в поле _id заказа", orders[0].get_id().isBlank());
+        assertFalse("В ответе вернулся пустое значение в поле _id заказа", orders[1].get_id().isBlank());
     }
 }
