0bd7c9ece6f938122b67c520f920862387d30482
Ekaterina Smagina <i.sealexa@yandex.ru>
2022-06-26 23:12:48 +0300

    rename CreateOrderNegativeTest to CreateOrderAuthNegativeTest

--- src/test/java/order/CreateOrderAuthNegativeTest.java
+++ src/test/java/order/CreateOrderAuthNegativeTest.java
@@ -15,12 +15,12 @@ import user.User;
 import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
-public class CreateOrderNegativeTest {
+public class CreateOrderAuthNegativeTest {
 
     private final String order;
     private final String description;
 
-    public CreateOrderNegativeTest(String order, String description){
+    public CreateOrderAuthNegativeTest(String order, String description){
         this.order = order;
         this.description = description;
     }
